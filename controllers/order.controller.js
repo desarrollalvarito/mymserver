4ee653f755f440671470e05bc613d9749c5a1992
mendozalvarito <desarrollalvarito@gmail.com>
2025-08-01 14:59:55 -0400

    add order list

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -4,9 +4,28 @@ import { generateToken } from '../helpers/token.helper.js'
 
 export const list = async (req, res) => {
     try {
-        let orders = await prisma.order.findMany()
+        let orderClient = await prisma.order.findMany({
+            include: {
+                client: true,
+            }
+        })
+        let quantity = await prisma.orderProduct.groupBy({
+            by: ['orderId'],
+            _sum: {
+                quantity: true
+            }
+
+        })
+        let orders = orderClient.map(o => {
+            let q = quantity.find(q => q.orderId === o.id)
+            return {
+                ...o,
+                quantity: q ? q._sum.quantity : 0
+            }
+        })
         return res.send(orders)
     } catch (error) {
+        console.log(error);
         return res.json({ error })
     }
 }
